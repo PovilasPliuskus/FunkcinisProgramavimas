e58102375cf0c9c719d50c218d78eb2ec3b24cd5
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-09-19 21:01:43 +0300

    Finish third function

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -8,7 +8,8 @@ module Lib1
   )
 where
 
-import DataFrame (DataFrame)
+import Data.List (transpose)
+import DataFrame (Column (..), ColumnType (..), DataFrame (..), Value (..))
 import InMemoryTables (TableName)
 
 type ErrorMessage = String
@@ -29,8 +30,38 @@ parseSelectAllStatement _ = error "parseSelectAllStatement not implemented"
 
 -- 3) implement the function which validates tables: checks if
 -- columns match value types, if rows sizes match columns,..
+
+-- checks if one column match value types
+checkColumnsValueTypes :: Column -> [Value] -> Bool
+checkColumnsValueTypes (Column _ expectedType) values =
+  all
+    ( \value ->
+        case (expectedType, value) of
+          (IntegerType, IntegerValue _) -> True
+          (StringType, StringValue _) -> True
+          (BoolType, BoolValue _) -> True
+          (_, NullValue) -> True
+          _ -> False
+    )
+    values
+
+-- checks if All the columns match value types
+checkAllColumnValueTypes :: DataFrame -> Bool
+checkAllColumnValueTypes (DataFrame columns values) =
+  all (\(column, columnValues) -> checkColumnsValueTypes column columnValues) (zip columns (transpose values))
+
+validateRows :: DataFrame -> Either ErrorMessage ()
+validateRows (DataFrame columns rows) =
+  if all (\row -> length row == length columns) rows
+    then Right ()
+    else Left "Row sizes do not match columns"
+
 validateDataFrame :: DataFrame -> Either ErrorMessage ()
-validateDataFrame _ = error "validateDataFrame ot implemented"
+validateDataFrame df@(DataFrame _ _) =
+  case (checkAllColumnValueTypes df, validateRows df) of
+    (True, Right ()) -> Right ()
+    (_, Left errMsg) -> Left errMsg
+    (False, _) -> Left "Columns have mismatched types"
 
 -- 4) implement the function which renders a given data frame
 -- as ascii-art table (use your imagination, there is no "correct"
